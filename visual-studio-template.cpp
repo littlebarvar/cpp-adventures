bd61e21316def7b1d024ce417c465fa1f2ffdfe7
unknown <kenny@machinesung.com>
2018-05-12 19:03:31 +0800

    First

---
+++ visual-studio-template.cpp
@@ -0,0 +1,24 @@
+// ConsoleApplication3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+inline void keep_window_open()
+{
+	char ch;
+	cin >> ch;
+}
+
+int main()
+{
+	cout << "Hello, Visual Studio!\n";
+	keep_window_open();
+	return 0;
+}
